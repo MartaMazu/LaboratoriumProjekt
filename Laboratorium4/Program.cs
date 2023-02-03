286f2fe1b3b5f6ee364e723b27bfd54f8c7cc3b8
Marta Mazurkiewicz 45471 <84677431+MartaMazu@users.noreply.github.com>
2023-02-03 22:57:41 +0100

    Refactoring

--- Laboratorium4/Program.cs
+++ Laboratorium4/Program.cs
@@ -26,12 +26,19 @@ public static class EquationSolver
    
     public static Tuple<double, double> Solve(double a, double b, double c)
     {
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant < 0)
+        double delta = b * b - 4 * a * c;
+
+        if (delta < 0)
+        {
             throw new Exception("Równanie nie posiada pierwiastków rzeczywistych.");
-        else if (discriminant == 0)
+        }
+        else if (delta == 0)
+        {
             return new Tuple<double, double>((-b) / (2 * a), double.NaN);
+        }
         else
-            return new Tuple<double, double>((-b + Math.Sqrt(discriminant)) / (2 * a), (-b - Math.Sqrt(discriminant)) / (2 * a));
+        {
+            return new Tuple<double, double>((-b + Math.Sqrt(delta)) / (2 * a), (-b - Math.Sqrt(delta)) / (2 * a));
+        }
     }
 }
