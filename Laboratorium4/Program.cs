7b6e96a1543b13e48eb7a8c5473a92dca4092e85
Marta Mazurkiewicz 45471 <84677431+MartaMazu@users.noreply.github.com>
2023-02-03 22:08:40 +0100

    Init commit

---
+++ Laboratorium4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
+
+public static class EquationSolver
+{
+   
+    public static Tuple<double, double> Solve(double a, double b, double c)
+    {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0)
+            throw new Exception("Równanie nie posiada pierwiastków rzeczywistych.");
+        else if (discriminant == 0)
+            return new Tuple<double, double>((-b) / (2 * a), double.NaN);
+        else
+            return new Tuple<double, double>((-b + Math.Sqrt(discriminant)) / (2 * a), (-b - Math.Sqrt(discriminant)) / (2 * a));
+    }
+}
---
+++ Laboratorium4Tests/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Laboratorium4Tests
+{
+    public class EquationSolverTests
+    {
+        [Theory]
+        [InlineData(1, -5, 6, 3, 2)]
+        [InlineData(1, -7, 10, 3, 2)]
+        [InlineData(1, 2, -3, 1, -3)]
+        public void Solve_PositiveTestCases_ReturnsCorrectRoots(double a, double b, double c, double root1, double root2)
+        {
+            var result = EquationSolver.Solve(a, b, c);
+
+            Assert.Equal(root1, result.Item1);
+            Assert.Equal(root2, result.Item2);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 2, -3)]
+        public void Solve_NegativeTestCases_ThrowsException(double a, double b, double c)
+        {
+            Assert.Throws<Exception>(() => EquationSolver.Solve(a, b, c));
+        }
+    }
+}
+
